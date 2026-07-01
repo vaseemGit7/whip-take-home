04c153e2346ad5c9ad89dd483d74e0b1841919a8
vaseemahamed30 <vahamed@velvee.ai>
2026-07-02 02:38:29 +0530

    chore: configure jest with transform rules for react-native packages

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-webview|@react-native-async-storage)/)',
+  ],
 };
